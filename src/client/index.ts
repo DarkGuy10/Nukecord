cd89059b9a1aa73870534d87aa38340e8b67cb0b
DarkGuy10 <HarperBurn@protonmail.ch>
2022-05-28 15:24:26 +0900

    refactor: add custom `NukecordClient` class

---
+++ src/client/index.ts
@@ -0,0 +1,27 @@
+import { Client, ClientOptions } from 'discord.js-selfbot-v13'
+
+/**
+ * The NukecordClient that extends djs-selfbot client
+ * Slightly modified to match our needs
+ */
+class NukecordClient extends Client {
+	constructor(props: ClientOptions) {
+		super(props)
+	}
+
+	/**
+	 * Wait for the client to get ready.
+	 * So we don't have to split the code into two parts.
+	 * Straight up butcher the event emitter lmao.
+	 */
+	async getReady(): Promise<void> {
+		const promise = new Promise<void>(resolve => {
+			this.on('ready', () => {
+				resolve()
+			})
+		})
+		return promise
+	}
+}
+
+export default NukecordClient
